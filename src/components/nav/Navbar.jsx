3d26a4a39bc2574b010076abe1db91cf452643df
ibrahim-sert <evo3506@gmail.com>
2023-03-18 17:15:20 +0300

    navbar finished

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, {  useState } from 'react'
+import Nav, { Hamburger, Imic, List, Navli } from './Navbar.style'
+import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
+
+const [isOpen, setIsOpen] = useState(false)
+
+
   return (
-    <div>Navbar</div>
+    <Nav justify="space-between" wrap="wrap">
+      <Imic to="/">
+        <i>{"<Abraham>"}</i>
+        <span>Recipe</span>
+      </Imic>
+      <Hamburger onClick={()=> setIsOpen(!isOpen)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+      <List>
+        <Navli to="/">Home</Navli>
+        <Navli to="/about">About</Navli>
+        <Navli to="/register">Register</Navli>
+        <Navli onClick={()=>sessionStorage.clear()} to="/">Logout</Navli>
+      </List>
+
+    </Nav>
   )
 }
 
